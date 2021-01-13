8589102ede63d82c7604858f18456e94aab24bd5
Praveen <pkchmits@gmail.com>
2021-01-13 09:00:11 +0530

    Added perform click listener to customView

--- app/src/main/java/com/sabkayar/praveen/customfancontroller/DialView.kt
+++ app/src/main/java/com/sabkayar/praveen/customfancontroller/DialView.kt
@@ -8,11 +8,21 @@ import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
 
- enum class FanSpeed(val label: Int) {
+enum class FanSpeed(val label: Int) {
     OFF(R.string.fan_off),
     LOW(R.string.fan_low),
     MEDIUM(R.string.fan_medium),
     HIGH(R.string.fan_high);
+
+    fun next() =
+        when (this) {
+            OFF -> LOW
+            LOW -> MEDIUM
+            MEDIUM -> HIGH
+            HIGH -> OFF
+        }
+
+
 }
 
 private const val RADIUS_OFFSET_LABEL = 30
@@ -24,6 +34,7 @@ class DialView @JvmOverloads constructor(
 
     private var radius = 0.0f                   // Radius of the circle.
     private var fanSpeed = FanSpeed.OFF         // The active selection.
+
     // position variable which will be used to draw label and indicator circle position
     private val pointPosition: PointF = PointF(0.0f, 0.0f)
 
@@ -32,14 +43,14 @@ class DialView @JvmOverloads constructor(
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
-        typeface = Typeface.create( "", Typeface.BOLD)
+        typeface = Typeface.create("", Typeface.BOLD)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-       radius=((min(w,h))/2.0*0.8).toFloat()
+        radius = ((min(w, h)) / 2.0 * 0.8).toFloat()
     }
 
-    private fun PointF.computeXYForSpeed(pos:FanSpeed,radius:Float){
+    private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
         // Angles are in radians.
         val startAngle = Math.PI * (9 / 8.0)
         val angle = startAngle + pos.ordinal * (Math.PI / 4)
@@ -50,15 +61,15 @@ class DialView @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         // Set dial background color to green if selection not off
-        paint.color=if(fanSpeed==FanSpeed.OFF) Color.GRAY else Color.GREEN
+        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
         // Draw the dial.
-        canvas.drawCircle((width/2).toFloat(),(height/2).toFloat(),radius,paint)
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
 
         // Draw the indicator circle.
         val markerRadius = radius + RADIUS_OFFSET_INDICATOR
         pointPosition.computeXYForSpeed(fanSpeed, markerRadius)
         paint.color = Color.BLACK
-        canvas.drawCircle(pointPosition.x, pointPosition.y, radius/12, paint)
+        canvas.drawCircle(pointPosition.x, pointPosition.y, radius / 12, paint)
 
 
         // Draw the text labels.
@@ -68,5 +79,19 @@ class DialView @JvmOverloads constructor(
             val label = resources.getString(i.label)
             canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
         }
+    }
+
+
+    init {
+        isClickable = true
+    }
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+        fanSpeed = fanSpeed.next()
+        contentDescription = resources.getString(fanSpeed.label)
+
+        invalidate()
+        return true
     }
 }
